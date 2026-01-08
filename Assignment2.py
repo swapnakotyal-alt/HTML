627c2b3307cbb6bcea8d54b2e438018e7e3abf6c
swapnakotyal-alt <swapnakotyal@gmail.com>
2026-01-08 10:21:52 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,6 +1,6 @@
 name = input("Enter your name: ")
 fav_number = input("Enter your favorite number: ")
-
 print("Hello", name, ", your favorite number is", fav_number)
 print("Type of name:", type(name))
 print("Type of favorite number:", type(fav_number))
+
